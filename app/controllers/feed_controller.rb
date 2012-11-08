2cb7d04b17cdf043a38612f1daccecfb960f8ddd
Steve Holm <steveholmphoto@me.com>
2012-11-08 16:06:20 -0600

    add icons to the image footer, add logo to the header, and change profile

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -13,13 +13,20 @@ class FeedController < ApplicationController
   def home  
     client = Instagram.client(:access_token => session[:access_token])
     @user = client.user
-    @recent = client.user_recent_media  
+    @recent = client.user_recent_media
     @popular = client.media_popular
     @location_search = client.location_recent_media(514276)
     # @resource_url = "https://api.instagram.com/v1/users/#{@user.id}/media/recent?access_token=#{session[:access_token]}"
     # @response = open(@resource_url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read
     # @get_pagination = JSON.parse(@response)
     # @get_next_url = @get_pagination["pagination"]["next_url"]
+
+    # u = IUser.new
+    # u.username = client.user.username
+    # u.fullname = client.user.full_name 
+    # if u.new_record?
+    # u.save
+    # end
   end
 
   def show
