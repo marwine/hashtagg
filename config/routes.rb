bfd9d49f0c7a78153f31c6473789e7bf6368c290
Mike Arwine <marwine@gmail.com>
2012-12-03 16:58:05 -0600

    Now able to push comments to instagram through user recent media modal

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ InstahashOauth::Application.routes.draw do
   get '/home' => 'feed#home', :as => :home
   get '/recent' => 'feed#recent', :as => :recent
   get '/show/:id' => 'feed#show', :as => :show
-  get '/home/test' => 'feed#test', :as => :test
+  put '/home/test' => 'feed#test', :as => :test
 
   get '/hashtags' => 'hashtag#index', :as => :albums
   post '/home/create_comment/:id' => 'feed#create', :as => :create_comment
