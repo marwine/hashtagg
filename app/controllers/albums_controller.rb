12c6c0fca16d2ed3af101ad5a4e13f53519a9f4f
Kelly <kellysteensma@gmail.com>
2012-12-08 15:22:51 -0600

    Completed Albums page and controller.  YAY.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,9 +5,27 @@ class AlbumsController < ApplicationController
     client = Instagram.client(:access_token => session[:access_token])
     @user = client.user
 
-    @albums = Album.all
     @user_id = User.find_by_instagram_id(@user["id"])["id"]
-    @album = Album.where(:user_id == @user_id)
+    @albums = Album.find_all_by_user_id(@user_id)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @albums }
+    end
+  end
+
+  # GET /albums/1
+  # GET /albums/1.json
+  def show
+    @album_name = params[:id]
+    @album = Album.where("name = '#{@album_name}'")
+
+    client = Instagram.client(:access_token => session[:access_token])
+    @user = client.user
+
+    # @albums = Album.all
+    # @user_id = User.find_by_instagram_id(@user["id"])["id"]
+    # @album = Album.where(:user_id == @user_id)
 
     @recent = []
     @page = "https://api.instagram.com/v1/users/#{@user["id"]}/media/recent?access_token=#{client.access_token}&count=60"
@@ -20,31 +38,18 @@ class AlbumsController < ApplicationController
       @page = "#{@pagination_call}&count=60"
     end   
 
-    @album_data = Hash.new { |hash, key| hash[key] = Array.new }
+    @album_data = Array.new []
 
     @recent.each do |instagram_record|
       instagram_record["tags"].each do |tag|
         @album.each do |one_album|
-          if tag == one_album['tag']
-            @album_data[tag] << instagram_record["images"]["standard_resolution"]["url"]
+          if tag == one_album["tag"]
+            @album_data << instagram_record["images"]["standard_resolution"]["url"]
           else ""
           end
         end
       end
-    end 
-
-# raise "#{@album_data.count}"
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @albums }
     end
-  end
-
-  # GET /albums/1
-  # GET /albums/1.json
-  def show
-    @album = Album.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -71,30 +76,19 @@ class AlbumsController < ApplicationController
   # POST /albums
   # POST /albums.json
   def create
-    @album = Album.new(params[:album])
+    client = Instagram.client(:access_token => session[:access_token])
+    @user = client.user
+    @album = params[:album]
+    @user_id = User.find_by_instagram_id(@user["id"])["id"]
+
+    @new_album = Album.new(:name => @album['name'], :tag => @album['tag'], :user_id => @user_id)
 
     respond_to do |format|
-      if @album.save
+      if @new_album.save
         format.html { redirect_to @album, notice: 'Album was successfully created.' }
         format.json { render json: @album, status: :created, location: @album }
       else
         format.html { render action: "new" }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /albums/1
-  # PUT /albums/1.json
-  def update
-    @album = Album.find(params[:id])
-
-    respond_to do |format|
-      if @album.update_attributes(params[:album])
-        format.html { redirect_to @album, notice: 'Album was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
         format.json { render json: @album.errors, status: :unprocessable_entity }
       end
     end
