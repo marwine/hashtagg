e4d29a6be11d684140f1bbdf6421ca2447754700
Mike Arwine <marwine@gmail.com>
2012-12-10 18:03:47 -0600

    updated callback url

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,7 +1,7 @@
 # LIVE 
 CLIENT_ID = "351eec6a508e427dae4ce0d709562a2c"
 CLIENT_SECRET = "4c96e800526d4137a9ec8de84123fc90"
-CALLBACK_URL = "http://hashtagg.herokuapp.com/session/callback"
+CALLBACK_URL = "http://www.hashtagg.in/session/callback"
 
 # DEV
 # CLIENT_ID = "c9b62cfe2e8a49b9bf9fae70ac5a81c8"
