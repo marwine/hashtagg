b7693bccdd88c956843d87a7e78c31c724f100c6
Mike Arwine <marwine@gmail.com>
2013-01-28 17:21:05 -0600

    Added keepalive in lib tasks

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,10 +1,10 @@
 # LIVE 
-CLIENT_ID = "351eec6a508e427dae4ce0d709562a2c"
-CLIENT_SECRET = "4c96e800526d4137a9ec8de84123fc90"
-CALLBACK_URL = "http://www.hashtagg.in/session/callback"
+# CLIENT_ID = "351eec6a508e427dae4ce0d709562a2c"
+# CLIENT_SECRET = "4c96e800526d4137a9ec8de84123fc90"
+# CALLBACK_URL = "http://www.hashtagg.in/session/callback"
 
 # DEV
-# CLIENT_ID = "c9b62cfe2e8a49b9bf9fae70ac5a81c8"
-# CLIENT_SECRET = "ada2b37091944493a114726e7c8386e0"
-# CALLBACK_URL = "http://localhost:3000/session/callback"
+CLIENT_ID = "c9b62cfe2e8a49b9bf9fae70ac5a81c8"
+CLIENT_SECRET = "ada2b37091944493a114726e7c8386e0"
+CALLBACK_URL = "http://localhost:3000/session/callback"
 
